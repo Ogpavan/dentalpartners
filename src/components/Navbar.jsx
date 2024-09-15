647ff0d77df9b795ee2015984e0502ddd50c7980
Ogpavan <pawantwp@gmail.com>
2024-09-15 22:51:15 +0530

    9/15/2024

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -260,6 +260,20 @@ function Navbar() {
                 >
                   Marketing Maintenance
                 </Link>
+                <Link
+                    to="/services/recruitment"
+                    className="block px-4 py-2 text-gray-800 hover:bg-gray-200 border-b border-gray-300"
+                    onClick={() => setDropdownOpen(false)}
+                  >
+                    Recruitment Services
+                  </Link>
+                  <Link
+                    to="/services/business"
+                    className="block px-4 py-2 text-gray-800 hover:bg-gray-200 rounded-b-lg"
+                    onClick={() => setDropdownOpen(false)}
+                  >
+                    Business Analytics
+                  </Link>
               </div>
             )}
             <Link
